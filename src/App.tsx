5a5a24ec3d92b3828ba6839b0376dfd1225706ae
LuigiToniolo <luigi.toniolo@domvsit.com.br>
2022-06-17 09:58:07 -0300

    Adicionando código no github

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
+import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
+import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
+import Home from './paginas/Home';
+import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+
+      <Route path='/admin' element={<PaginaBaseAdmin />}>
+
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+
+      </Route>
+
+    </Routes>
+  );
+}
+
+export default App;
